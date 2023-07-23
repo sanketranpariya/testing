27bbdec929eb0d67a32eecc3e7206fd60cf3c31a
Sanket Ranpariya <patelsanket7261@gmail.com>
2023-07-24 04:38:20 +0530

    cors added

--- app.js
+++ app.js
@@ -2,23 +2,29 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 const quizModel = require('./models/usermodel')
 const validator = require('./middleware/validationOne')
 const port = 3700
 
 //*middleware
+app.use(cors())
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
 //*connection request to DB
 const connectionReq = async ()=>{
-    const responseDb = await  mongoose.connect(process.env.MONGO_DB_CONNECTION)
-    if(responseDb){
-        console.log("DB Connected");
-    }else{
-        console.log(responseDb);
+    try {
+        const responseDb = await  mongoose.connect(process.env.MONGO_DB_CONNECTION)
+        if(responseDb){
+            console.log("DB Connected");
+        } else {
+            console.log("Some unusual thing happened during DB Connection");
+        }
+        return;
+    } catch (error) {
+        return console.log(error);
     }
-    return;
 }
 connectionReq();
 
